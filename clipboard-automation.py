df6fdff0e7b1b24846d657d5896cba605c8cc03f
Nicol69 <kandelnicol@gmail.com>
2022-07-18 00:43:56 +0545

    This is an last commit that save loads and list user key and value in json"

--- clipboard-automation.py
+++ clipboard-automation.py
@@ -1,5 +1,4 @@
 import sys
-from tkinter.filedialog import SaveAs
 import clipboard
 import json
 
@@ -9,30 +8,51 @@ import json
 #print(sys.argv[1:])
 
 # if the length of arguments is 2 then printing the argument in the form of list that has index  position 1 i.e example:hello
+
+SAVED_DATA = "multiclipboard.json"
+
 def save_info(filepath,info):
     with open(filepath,"w") as f:
         json.dump(info,f)
-save_info("testing.json",{"employee":"paid"})
 
 #Loading or reading saved file as
 def load_item(filepath):
-    with open(filepath,"r") as f:
-        info =json.load(f)
-        return info
-    
+    try:
+        with open(filepath,"r") as f:
+            info =json.load(f)
+            return info
+    except:
+        return {}
+
 
 if len(sys.argv)==2:
     data= sys.argv[1]
+    info = load_item(SAVED_DATA)
+
+# Used to save key and value pair
+
     if data == "save":
-        print("Saved")
+        key = input("Enter a key:")
+        info[key] = clipboard.paste()
+        save_info(SAVED_DATA,info)
+        print("KEY SAVED SUCCESSFULLY!!")
+
     elif data == "load":
-        print("Loaded")
+        key = input("Enter key:")
+        if key in info:
+            clipboard.copy(info[key])
+            print("Info Copied Successfully!!")
+        else:
+            print("KEY DOESN'T EXISTS !!")
+
     elif data=="list":
-        print("Listed")
+        print(info)
+
     else:
-        print("Unknown command")
+        print("Unknown Command!!")
+
 else:
-    print("Command length must not exceed 2")
+    print("PLEASE ENTER ONE COMMAND AT A TIME!!")
     
 
 
