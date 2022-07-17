10e2a0de85061fcc7ea7a2925c757cca22ff429b
Nicol69 <kandelnicol@gmail.com>
2022-07-17 22:45:40 +0545

    First commit on multiclipboard

---
+++ clipboard-automation.py
@@ -0,0 +1,20 @@
+import sys
+import clipboard
+import json
+
+
+# argv is a list in Python that contains all the command-line arguments passed to the script. 
+
+#print(sys.argv[1:])
+
+# if the length of arguments is 2 then printing the argument in the form of list that has index  position 1 i.e example:hello
+# if len(sys.argv)==4:
+#     data=sys.argv[1:]
+#     print(data)
+
+
+
+
+
+
+ 
