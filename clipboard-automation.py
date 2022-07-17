de43bd8363af50e87fc569e583b78a7154a9574f
Nicol69 <kandelnicol@gmail.com>
2022-07-17 23:22:09 +0545

    Second Commit On Save,Load,List

--- clipboard-automation.py
+++ clipboard-automation.py
@@ -8,11 +8,30 @@ import json
 #print(sys.argv[1:])
 
 # if the length of arguments is 2 then printing the argument in the form of list that has index  position 1 i.e example:hello
-# if len(sys.argv)==4:
-#     data=sys.argv[1:]
-#     print(data)
+def save_info(filepath,info):
+    with open(filepath,"w") as f:
+        json.dump(info,f)
+save_info("testing.json",{"employee":"paid"})
 
 
+if len(sys.argv)==2:
+    data= sys.argv[1]
+    if data == "save":
+        print("Saved")
+    elif data == "load":
+        print("Loaded")
+    elif data=="list":
+        print("Listed")
+    else:
+        print("Unknown command")
+else:
+    print("Command length must not exceed 2")
+    
+
+
+
+
+    
 
 
 
